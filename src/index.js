37e2e2599bf0463ec5cb71c878f0c84ff8e847a3
olenapolko <olenapolko@gmail.com>
2023-04-07 00:57:37 +0300

    added hash-router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {HashRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
+
 
 import { ProductsProvider } from './context/products_context';
 import { FilterProvider } from './context/filter_context';
@@ -12,20 +14,22 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <Auth0Provider
-        domain={process.env.REACT_APP_AUTH0_DOMAIN}
-        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-        authorizationParams={{ redirect_uri: window.location.origin }}
-        cacheLocation='localstorage'
-    >
-        <UserProvider>
-            <ProductsProvider>   
-                <FilterProvider>
-                    <CartProvider>
-                        <App />
-                    </CartProvider>
-                </FilterProvider>
-            </ProductsProvider>
-        </UserProvider>
-    </Auth0Provider>
+    <HashRouter>
+        <Auth0Provider
+            domain={process.env.REACT_APP_AUTH0_DOMAIN}
+            clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+            authorizationParams={{ redirect_uri: window.location.origin }}
+            cacheLocation='localstorage'
+        >
+            <UserProvider>
+                <ProductsProvider>   
+                    <FilterProvider>
+                        <CartProvider>
+                            <App />
+                        </CartProvider>
+                    </FilterProvider>
+                </ProductsProvider>
+            </UserProvider>
+        </Auth0Provider>
+    </HashRouter>
 );

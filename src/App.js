619a16a4db34e590c2fa869a2876d8166be0e056
olenapolko <olenapolko@gmail.com>
2023-04-08 19:59:13 +0300

    upgraded router version

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
-import {Home, SingleProduct, Cart, Checkout, Error, About, Products, PrivateRoute, AuthWrapper} from './pages'
+import {Home, SingleProduct, Cart, Checkout, Error, About, Products, AuthWrapper} from './pages'
 
 function App() {
   return (
     <AuthWrapper>
-      <Router>
         <Navbar />
         <Sidebar/>
-        <Switch>
-          <Route exact path='/'>
-            <Home/>
-          </Route>
-          <Route exact path='/about'>
-            <About/>
-          </Route>
-          <Route exact path='/cart'>
-            <Cart/>
-          </Route>
-          <Route exact path='/products'>
-            <Products />
-          </Route>
-          <Route exact path='/products/:id' children={<SingleProduct/>} />
-          <PrivateRoute exact path='/checkout'>
-            <Checkout/>
-          </PrivateRoute>
-          <Route path='*'>
-            <Error/>
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/products/:id' element={<SingleProduct/>} />
+          <Route path='/checkout' element={<Checkout/>} />
+          <Route path='*' element={<Error/>}/>
+        </Routes>
         <Footer/>
-        </Router>
       </AuthWrapper>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HashRouter} from 'react-router-dom';
+import {createHashRouter, RouterProvider} from 'react-router-dom';
 import './index.css';
 import App from './App';
-
 
 import { ProductsProvider } from './context/products_context';
 import { FilterProvider } from './context/filter_context';
@@ -11,10 +10,16 @@ import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createHashRouter([
+    {
+      path: "/*",
+      element: <App />,
+    }
+]);  
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+  
 root.render(
-    <HashRouter>
         <Auth0Provider
             domain={process.env.REACT_APP_AUTH0_DOMAIN}
             clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
@@ -25,11 +30,10 @@ root.render(
                 <ProductsProvider>   
                     <FilterProvider>
                         <CartProvider>
-                            <App />
+                            <RouterProvider router={router}></RouterProvider>
                         </CartProvider>
                     </FilterProvider>
                 </ProductsProvider>
             </UserProvider>
         </Auth0Provider>
-    </HashRouter>
 );

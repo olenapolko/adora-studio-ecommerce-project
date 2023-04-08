c0a14726f33cf46fdc7db89cac2d82c56ba8ba1e
olenapolko <olenapolko@gmail.com>
2023-04-08 21:47:55 +0300

    improved auth provider params

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import {createHashRouter, RouterProvider} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+
 import { ProductsProvider } from './context/products_context';
 import { FilterProvider } from './context/filter_context';
 import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const router = createHashRouter([
     {
@@ -16,15 +19,12 @@ const router = createHashRouter([
       element: <App />,
     }
 ]);  
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
   
 root.render(
         <Auth0Provider
             domain={process.env.REACT_APP_AUTH0_DOMAIN}
             clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-            authorizationParams={{ redirect_uri: window.location.origin }}
-            cacheLocation='localstorage'
+            authorizationParams={{ redirect_uri: window.location.origin}}
         >
             <UserProvider>
                 <ProductsProvider>   

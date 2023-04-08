4d3fee1a1b6d15e6f0cfd42e0d1ba057fbfd67cd
olenapolko <olenapolko@gmail.com>
2023-04-08 23:04:55 +0300

    changed redirect_uri

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ root.render(
         <Auth0Provider
             domain={process.env.REACT_APP_AUTH0_DOMAIN}
             clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-            authorizationParams={{ redirect_uri: window.location.origin}}
+            authorizationParams={{ redirect_uri: process.env.REACT_APP_AUTH0_REDIRECT_URI}}
         >
             <UserProvider>
                 <ProductsProvider>   
